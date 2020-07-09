4aeafe52c09a1cb56ab3ebe3ea517e9f532b37e9
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-10 01:29:56 +0900

    Refactor boot

--- src/renderer/boot.ts
+++ src/renderer/boot.ts
@@ -5,59 +5,88 @@ import fs from 'fs';
 import { remote, MenuItem, BrowserWindow } from 'electron';
 
 import Env from '@/stores/Env';
+import EnemyRepository from '@/dao/Enemy/Repository';
+import EnemyEntity from './dao/Enemy/Entity';
 
 /******************************************************************************
  * アプリケーションメニューの処理
  *****************************************************************************/
 const { Menu, dialog } = remote;
 
+/**
+ * ディレクトリを選択してデータを読み込む
+ */
 const open = async (item:MenuItem, win:BrowserWindow) => 
 {
   // ダイアログが開いている間はWindowを操作できなくする
   win.setEnabled(false);
   Env.lock();
 
-  // ディレクトリ選択ダイアログを開く
-  const result = await dialog.showOpenDialog({ properties: ['openDirectory'] });
+  try {
 
-  // キャンセル
-  if (result.canceled) {
-    Env.unlock();
-    win.setEnabled(true);
-    return;
-  }
+    // ディレクトリ選択ダイアログを開く
+    const result = await dialog.showOpenDialog({ properties: ['openDirectory'] });
 
-  // パスを保存
-  Env.path = result.filePaths[0];
-  win.setTitle(Env.path);
-    EnemyRepository.deserialize(json, EnemyEntity);
-
-  Env.unlock();
-  win.setEnabled(true);
-}
-  
-const save = async (item:MenuItem, win:BrowserWindow) => {
-
-  win.setEnabled(false);
-  Env.lock();
-
-  // パスがなければ保存先のパスを選択させる
-  while (!fs.existsSync(Env.path)) 
-  {
-    const result = await dialog.showOpenDialog({ properties: ['openDirectory']});
-    
-
-    if (result.canceled) {
+    // ディレクトリ選択がキャンセルされた
+    if (result.canceled) 
+    {
       Env.unlock();
       win.setEnabled(true);
       return;
     }
 
+    // パスを保存
     Env.path = result.filePaths[0];
-  }
 
-  Env.unlock();
-  win.setEnabled(true);
+    // Windowタイトルにパスを設定
+    win.setTitle(Env.path);
+
+    // ファイル読み込み
+    const json = fs.readFileSync(Env.path + "\\enemy.json", 'utf8');
+    EnemyRepository.deserialize(json, EnemyEntity);
+
+  } catch(err) {
+    console.log(err);
+    alert("ファイルの読み込みに失敗しました。");
+  } finally {
+    Env.unlock();
+    win.setEnabled(true);    
+  }
+}
+  
+/**
+ * ディレクトリを選択してデータを書き込む
+ */
+const save = async (item:MenuItem, win:BrowserWindow) => {
+
+  win.setEnabled(false);
+  Env.lock();
+
+  try {
+
+    // パスがなければ保存先のパスを選択させる
+    if (!fs.existsSync(Env.path)) 
+    {
+      const result = await dialog.showOpenDialog({ properties: ['openDirectory']});
+      
+
+      if (result.canceled) {
+        Env.unlock();
+        win.setEnabled(true);
+        return;
+      }
+      Env.path = result.filePaths[0];
+    }
+
+    fs.writeFileSync(Env.path + "\\enemy.json", EnemyRepository.serialize());
+
+  } catch(err) {
+    console.log(err);
+    alert("ファイルの保存に失敗しました。");    
+  } finally {
+    Env.unlock();
+    win.setEnabled(true);
+  }
 }
 
 const create = () => {
